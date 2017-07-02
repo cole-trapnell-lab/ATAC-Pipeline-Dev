09c0989d8481e046a2869920775780557e0314ed
Hannah Pliner <hpliner@gmail.com>
2017-07-02 16:46:48 -0700

    fix dedup to darrens

--- src/sc_atac_true_dedup.py
+++ src/sc_atac_true_dedup.py
@@ -1,5 +1,4 @@
-#!/net/gs/vol1/home/hpliner/miniconda2/bin/python
-import pysam   #/0.8.1
+import pysam #v0.8.1
 import sys
 
 inbam = sys.argv[1]
@@ -9,15 +8,21 @@ readsin = pysam.AlignmentFile(inbam,"rb")
 readsout = pysam.AlignmentFile(outbam,"wb",template=readsin)
 refs = readsin.references
 for refchrom in refs:
+	if 'chrM' in refchrom or 'chrGL' in refchrom or 'chrNC' in refchrom or 'chrhs' in refchrom or 'random' in refchrom or 'chrU' in refchrom:
+		continue
 	readdic = {}
 	print "Deduplicating " + refchrom + "..."
 	for read in readsin.fetch(refchrom):
 		readname = read.qname.split(':')[0]
-		readpos = str(read.pos + 1)
-		matepos = str(read.mpos)
+		if read.tlen < 0:
+			fragstart = str(read.mpos - read.tlen)
+			fragend = str(read.mpos)
+		else:
+			fragstart = str(read.pos)
+			fragend = str(read.pos + read.tlen)
 		try:
-			readdic[readname + readpos + matepos]
+			readdic[readname + fragstart + fragend]
 		except KeyError:
-			readdic[readname + readpos + matepos] = read
+			readdic[readname + fragstart + fragend] = read
 			readsout.write(read)
 
